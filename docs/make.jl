bb5d8b7b82f6a7aab64b93403d98fb9a966fe152
schillic <git@christianschilling.net>
2021-12-12 16:26:05 +0100

    simpler style file

--- docs/make.jl
+++ docs/make.jl
@@ -8,7 +8,7 @@ makedocs(
     modules = [JuliaReachTemplatePkg],
     format = Documenter.HTML(
         prettyurls = get(ENV, "CI", nothing) == "true",
-        assets = ["assets/juliareach-light.css"]),
+        assets = ["assets/aligned.css"]),
     pages = [
         "Home" => "index.md",
         "About" => "about.md"
