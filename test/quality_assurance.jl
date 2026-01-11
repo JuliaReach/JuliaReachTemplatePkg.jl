becd961b06f8c2f658f8b7fff96bfd851bf83f6c
schillic <git@christianschilling.net>
2026-01-11 13:24:28 +0100

    use ExplicitImports in tests

--- test/quality_assurance.jl
+++ test/quality_assurance.jl
@@ -1,5 +1,15 @@
 using JuliaReachTemplatePkg, Test
-import Aqua
+import Aqua, ExplicitImports
+
+@testset "ExplicitImports tests" begin
+    @test isnothing(ExplicitImports.check_all_explicit_imports_are_public(JuliaReachTemplatePkg))
+    @test isnothing(ExplicitImports.check_all_explicit_imports_via_owners(JuliaReachTemplatePkg))
+    @test isnothing(ExplicitImports.check_all_qualified_accesses_are_public(JuliaReachTemplatePkg))
+    @test isnothing(ExplicitImports.check_all_qualified_accesses_via_owners(JuliaReachTemplatePkg))
+    @test isnothing(ExplicitImports.check_no_implicit_imports(JuliaReachTemplatePkg))
+    @test isnothing(ExplicitImports.check_no_self_qualified_accesses(JuliaReachTemplatePkg))
+    @test isnothing(ExplicitImports.check_no_stale_explicit_imports(JuliaReachTemplatePkg))
+end
 
 @testset "Aqua tests" begin
     Aqua.test_all(JuliaReachTemplatePkg; project_extras=false)
